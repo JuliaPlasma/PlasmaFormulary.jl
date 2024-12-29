e9bda588127e236fad529f3ef9f19c780567ce3d
Luke Adams <luke@lukeclydeadams.com>
2024-12-29 16:39:02 -0700

    add permute args macro to plasma beta

--- src/dimensionless.jl
+++ src/dimensionless.jl
@@ -8,7 +8,8 @@ Compute the plamsa beta (β), the ratio of thermal pressure to magnetic pressure
 - `n`: The particle density of the plasma.
 - `B`: The magnetic field in the plasma.
 """
-function plasma_beta(T::EnergyOrTemp, n::NumberDensity, B::Unitful.BField)
+function plasma_beta end
+@permute_args function plasma_beta(T::EnergyOrTemp, n::NumberDensity, B::Unitful.BField)
     p_th = thermal_pressure(T, n)
     p_B = magnetic_pressure(B)
     return p_th / p_B |> upreferred
