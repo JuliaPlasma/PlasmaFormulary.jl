91932e735bb23244e06ef629b8b01992e0329036
Beforerr <zzj956959688@gmail.com>
2025-01-12 00:06:06 -0800

    CI: add gyrofrequency test

--- test/frequencies.jl
+++ test/frequencies.jl
@@ -1,4 +1,6 @@
 @testset "Frequency" begin
+
+    @test gyrofrequency(0.01u"T", :p) ≈ 957883.3292211705u"s^-1"
     @test plasma_frequency(1e19u"m^-3", :p) ≈ 4163294534.0u"s^-1"
     @test plasma_frequency(1e19u"m^-3") ≈ 1.783986365e11u"s^-1"
 end
