7c34ade8c40cd6769ac886436d2048841969acba
Beforerr <zzj956959688@gmail.com>
2025-02-04 11:41:09 -0800

    fix: result should return angular frequency instead of frequency

--- src/frequencies.jl
+++ src/frequencies.jl
@@ -3,7 +3,7 @@
     gyrofrequency(B::BField, mass::Mass, q::Charge)
 
 Calculate the gyrofrequency (or cyclotron frequency) of a charged particle's circular motion in a magnetic field.
-The gyrofrequency is the frequency of a charged particle's gyromotion around magnetic field lines.
+The gyrofrequency is the angular frequency of a charged particle's gyromotion around magnetic field lines.
 
 References: 
 - [PlasmaPy Documentation](https://docs.plasmapy.org/en/latest/api/plasmapy.formulary.frequencies.gyrofrequency.html)
@@ -11,19 +11,19 @@ References:
 # Examples
 ```jldoctest; filter = r"(\\^-1|⁻¹)"
 julia> gyrofrequency(0.01u"T", :p)  # proton gyrofrequency
-957883.3292211705 s⁻¹
+957883.3292211705 rad s⁻¹
 
-julia> gyrofrequency(0.1u"T", :e)  # electron gyrofrequency
-1.7588200107721634e10 s⁻¹
+julia> uconvert(u"Hz", gyrofrequency(0.1u"T", :e), Periodic())  # electron gyrofrequency as frequency
+2.799248987233304e9 Hz
 
 julia> gyrofrequency(250u"Gauss", "Fe"; z=13)  # Fe2+ ion gyrofrequency
-560682.3520611608 s⁻¹
+560682.3520611608 rad s⁻¹
 ```
 """
 function gyrofrequency end
 
 @permute_args function gyrofrequency(B::BField, mass::Mass, q::Charge)
-    upreferred(abs(q * B / mass))
+    upreferred(rad * abs(q * B / mass))
 end
 
 @permute_args function gyrofrequency(B::BField, p::ParticleLike; kw...)
@@ -43,10 +43,10 @@ More often, it refers to the frequency at which electrons oscillate in the plasm
 # Examples
 ```jldoctest; filter = r"(\\^-1|⁻¹)"
 julia> plasma_frequency(1e19u"m^-3")  # plasma frequency
-1.7839863654934653e11 s⁻¹
+1.7839863654934653e11 rad s⁻¹
 
 julia> plasma_frequency(1e19u"m^-3", :p)  # proton plasma frequency
-4.1632945624883513e9 s⁻¹
+4.1632945624883513e9 rad s⁻¹
 ```
 
 # References
@@ -56,7 +56,7 @@ julia> plasma_frequency(1e19u"m^-3", :p)  # proton plasma frequency
 function plasma_frequency end
 
 @permute_args function plasma_frequency(n::NumberDensity, q::Charge, mass::Mass)
-    abs(q) * sqrt(n / mass / ε0) |> upreferred
+    rad * abs(q) * sqrt(n / mass / ε0) |> upreferred
 end
 
 @permute_args function plasma_frequency(n::NumberDensity, p::ParticleLike; kw...)
--- test/frequencies.jl
+++ test/frequencies.jl
@@ -1,6 +1,11 @@
-@testset "Frequency" begin
+using PlasmaFormulary
+using Test
+using Unitful
+using DimensionfulAngles
 
-    @test gyrofrequency(0.01u"T", :p) ≈ 957883.3292211705u"s^-1"
-    @test plasma_frequency(1e19u"m^-3", :p) ≈ 4163294534.0u"s^-1"
-    @test plasma_frequency(1e19u"m^-3") ≈ 1.783986365e11u"s^-1"
+@testset "Frequency" begin
+    @test gyrofrequency(0.01u"T", :p) ≈ 957883.3292211705u"radᵃ*s^-1"
+    @test uconvert(u"Hz", gyrofrequency(0.1u"T", :e), Periodic()) ≈ 2.799248987233304e9u"Hz"
+    @test plasma_frequency(1e19u"m^-3", :p) ≈ 4163294534.0u"radᵃ*s^-1"
+    @test plasma_frequency(1e19u"m^-3") ≈ 1.783986365e11u"radᵃ*s^-1"
 end
