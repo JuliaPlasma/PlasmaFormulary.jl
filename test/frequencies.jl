296994d8c9f152b42ca08f7ccbe2abf7ee439098
Zijin Zhang <58776897+Beforerr@users.noreply.github.com>
2025-05-04 23:10:01 -0700

    CI: fix and move to TestItem framework (#3)
    * CI: refactor using TestItems
    
    * CI: use lts and release version
    
    * CI: fix setup

--- test/frequencies.jl
+++ test/frequencies.jl
@@ -1,7 +1,6 @@
-using Unitful
-
 @testitem "Frequency" begin
     using DimensionfulAngles
+    using Unitful
     @test gyrofrequency(0.01u"T", :p) ≈ 957883.3292211705u"radᵃ*s^-1"
     @test uconvert(u"Hz", gyrofrequency(0.1u"T", :e), Periodic()) ≈ 2.799248987233304e9u"Hz"
     @test plasma_frequency(1e19u"m^-3", :p) ≈ 4163294534.0u"radᵃ*s^-1"
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,52 +1,48 @@
-using PlasmaFormularyZ
-using Test
 using TestItems, TestItemRunner
-using Unitful
-using LinearAlgebra
-using PythonCall
 using CondaPkg
-
 CondaPkg.add(["astropy", "plasmapy"])
 
-include("test_utils.jl")
+@testsnippet Py begin
+    using PythonCall
+    include("test_utils.jl")
+    units = pyimport("astropy.units")
+    formulary = pyimport("plasmapy.formulary")
+end
 
-units = pyimport("astropy.units")
-formulary = pyimport("plasmapy.formulary")
+@run_package_tests
 
-@testset "PlasmaFormularyZ.jl" begin
-    @testset "Misc" begin
-        @test gyroradius(0.2u"T", Unitful.mp, Unitful.q, 1e6u"K") ≈ 0.0067u"m" rtol = 0.01
-        @test_broken electron_gyroradius(1e6u"K", 0.2u"T", Unitful.q, Unitful.mp) ≈
-                     0.0067u"m" rtol = 0.01
+@testitem "Misc" begin
+    using Unitful
+    using LinearAlgebra
+    @test gyroradius(0.2u"T", Unitful.mp, Unitful.q, 1e6u"K") ≈ 0.0067u"m" rtol = 0.01
+    @test_broken electron_gyroradius(1e6u"K", 0.2u"T", Unitful.q, Unitful.mp) ≈ 0.0067u"m" rtol =
+        0.01
 
-        B = [-0.014u"T", 0.0u"T", 0.0u"T"]
-        Bmag = norm(B)
-        n = 5e19 * u"m^-3"
-        mass_number = 1
-        rho = n * mass_number * Unitful.mp
-        @test norm(alfven_velocity.(B, rho)) ==
-              alfven_velocity(Bmag, rho) ==
-              alfven_velocity(Bmag, n, mass_number) ≈
-              43185.625u"m/s"
+    B = [-0.014u"T", 0.0u"T", 0.0u"T"]
+    Bmag = norm(B)
+    n = 5e19 * u"m^-3"
+    mass_number = 1
+    rho = n * mass_number * Unitful.mp
+    @test norm(alfven_velocity.(B, rho)) ==
+          alfven_velocity(Bmag, rho) ==
+          alfven_velocity(Bmag, n, mass_number) ≈
+          43185.625u"m/s"
 
-        @test alfven_velocity.(B, rho) ≈ [-43185.625u"m/s", 0.0u"m/s", 0.0u"m/s"]
-    end
+    @test alfven_velocity.(B, rho) ≈ [-43185.625u"m/s", 0.0u"m/s", 0.0u"m/s"]
+end
 
-    @testset "dimensionless.jl" begin
-        T = 2.0u"eV"
-        n = 1e19u"m^-3"
-        B = 0.1u"T"
-        @test plasma_beta(T, n, B) ==
-              plasma_beta(n, B, PlasmaFormularyZ.temperature(T)) ==
-              plasma_beta(n, T, B)
+@testitem "dimensionless.jl" setup = [Py] begin
+    using Unitful
 
-        @test pyustrip(
-            units.m,
-            formulary.lengths.Debye_length(10 * units.eV, 1e19 / units.m^3),
-        ) ≈ ustrip(u"m", PlasmaFormularyZ.debye_length(1e19 * u"m^-3", 10 * u"eV")) rtol =
-            1e-3
-    end
+    T = 2.0u"eV"
+    n = 1e19u"m^-3"
+    B = 0.1u"T"
+    @test plasma_beta(T, n, B) ==
+          plasma_beta(n, B, PlasmaFormularyZ.temperature(T)) ==
+          plasma_beta(n, T, B)
 
-    include("frequencies.jl")
-    include("length.jl")
+    @test pyustrip(
+        units.m,
+        formulary.lengths.Debye_length(10 * units.eV, 1e19 / units.m^3),
+    ) ≈ ustrip(u"m", PlasmaFormularyZ.debye_length(1e19 * u"m^-3", 10 * u"eV")) rtol = 1e-3
 end
